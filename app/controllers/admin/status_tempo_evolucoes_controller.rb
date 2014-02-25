542ef834317bf6ef16a4b7ec68fa72a5a941f83a
Deivisson <deivisson.bruno@gmail.com>
2014-02-25 00:08:55 -0300

    Cadastro de Tempo de Evolução dos Status

--- app/controllers/admin/status_tempo_evolucoes_controller.rb
+++ app/controllers/admin/status_tempo_evolucoes_controller.rb
@@ -1,25 +1,15 @@
 # -*- encoding : utf-8 -*-
 class Admin::StatusTempoEvolucoesController < Admin::BaseController
+  before_filter :carrega_tempo_evolucao, :only => [:edit,:destroy,:update]
   before_filter :load_status, :only => [:create]
   before_filter :carrega_dados, :except => [:index,:show]
-  # def index
-  #   @status_tempo_evolucoes = StatusTempoEvolucao.all
-  #   respond_with(@status_tempo_evolucoes)
-  # end
-
-  # def show
-  #   @status_tempo_evolucao = StatusTempoEvolucao.find(params[:id])
-  #   respond_with(@status_tempo_evolucao)
-  # end
 
   def new
     @status_tempo_evolucao = StatusTempoEvolucao.new
     respond_with(@status_tempo_evolucao)
   end
 
-  def edit
-    @status_tempo_evolucao = StatusTempoEvolucao.find(params[:id])
-  end
+  def edit; end
 
   def create
     @status_tempo_evolucao = @status.tempo_evolucoes.build(params[:status_tempo_evolucao])
@@ -30,18 +20,21 @@ class Admin::StatusTempoEvolucoesController < Admin::BaseController
   end
 
   def update
-    @status_tempo_evolucao = StatusTempoEvolucao.find(params[:id])
     @status_tempo_evolucao.update_attributes(params[:status_tempo_evolucao])
-    respond_with(@status_tempo_evolucao)
+    respond_with(@status_tempo_evolucao,
+        location:admin_status_path(@status_tempo_evolucao.status))
   end
 
   def destroy
-    @status_tempo_evolucao = StatusTempoEvolucao.find(params[:id])
     @status_tempo_evolucao.destroy
-    respond_with(@status_tempo_evolucao)
+    respond_with(@status_tempo_evolucao, 
+        location:admin_status_path(@status_tempo_evolucao.status))
   end
 
 private 
+  def carrega_tempo_evolucao
+    @status_tempo_evolucao = StatusTempoEvolucao.find(params[:id])  
+  end
 
   def load_status
     @status = Status.find(params[:status_id])
