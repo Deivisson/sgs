7f65dd0f5e8445a69f00ea24a0c1db0a567e3aa6
Deivisson <deivisson.bruno@gmail.com>
2014-11-26 22:24:24 -0200

    Alterações 6

--- app/controllers/usuario/clientes_controller.rb
+++ app/controllers/usuario/clientes_controller.rb
@@ -10,11 +10,15 @@ class Usuario::ClientesController < Usuario::CadastrosBasicosController
   before_filter :carrega_dados,     :except => [:index,:destroy,:show]  
 
   def index
-    @clientes = Cliente.where("id > 0")
+    @clientes = Cliente.where("id > 0").includes(:categoria)
     if @conteudo_busca_cliente.present?
       @clientes = @clientes.where("(nome like :a or apelido like :a or cnpj like :a)",a:"%#{@conteudo_busca_cliente}%")
     end
+    if @categoria_cliente.to_i > 0
+      @clientes = @clientes.where("categoria_cliente_id = ? ", @categoria_cliente)
+    end
     @clientes = @clientes.order(:nome).paginate :page => @page, :per_page =>25
+    @categoria_clientes = CategoriaCliente.order(:descricao)
     respond_with(@clientes)
   end
 
@@ -76,7 +80,17 @@ private
       params.delete(:conteudo_busca_cliente)
       @page = nil
     end
+
+    @categoria_cliente = 0
+    if params[:categoria_cliente].nil?
+      @categoria_cliente = session[:categoria_cliente] || ""
+    elsif params[:categoria_cliente].present?
+      @categoria_cliente = params[:categoria_cliente]
+      params.delete(:categoria_cliente)
+      @page = nil
+    end    
     session[:conteudo_busca_cliente] = @conteudo_busca_cliente
+    session[:categoria_cliente] = @categoria_cliente
     session[:page_cliente] = @page
   end
 
